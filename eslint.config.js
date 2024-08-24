dd22e395a11b464a68901ba33f8d4f626dc84042
Guido Falsi <mad@madpilot.net>
2024-08-24 15:31:29 +0200

    Adapt configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,21 @@
+import babelEslintParser from '@babel/eslint-parser'
+import babelPluginSyntaxImportAttributes from '@babel/plugin-syntax-import-assertions'
 import madpilotEslintConfigBase from '@madpilot78/eslint-config-base'
 
 export default [
   ...madpilotEslintConfigBase,
   {
-    extends: ['@madpilot78/eslint-config-base'],
-    globals: {
-      module: 'readonly'
+    languageOptions: {
+      parser: babelEslintParser,
+      parserOptions: {
+        babelOptions: {
+          plugins: [babelPluginSyntaxImportAttributes]
+        },
+        requireConfigFile: false
+      },
+      globals: {
+        module: 'readonly'
+      }
     }
   }
 ]
