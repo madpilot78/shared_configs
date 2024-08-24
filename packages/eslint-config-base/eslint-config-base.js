750bb014a0b1782825f5e98870e0f5817cf9c15c
Guido Falsi <mad@madpilot.net>
2024-08-24 14:56:26 +0200

    Update to new flat configuration for ESLint.
    Bump requirements.

--- packages/eslint-config-base/eslint-config-base.js
+++ packages/eslint-config-base/eslint-config-base.js
@@ -1,83 +1,87 @@
-module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:prettier/recommended',
-    'plugin:promise/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['@getify/proper-arrows', 'promise'],
-  rules: {
-    '@getify/proper-arrows/params': [
-      'error',
-      {
-        unused: 'trailing',
-        count: 3,
-        length: 0
-      }
-    ],
-    '@getify/proper-arrows/this': ['warn', 'never'],
-    'arrow-body-style': ['error', 'as-needed'],
-    'arrow-parens': ['warn'],
-    'comma-dangle': ['error', 'never'],
-    curly: ['error', 'all'],
-    'default-case-last': ['error'],
-    'dot-notation': ['error'],
-    eqeqeq: ['error', 'always'],
-    'no-array-constructor': ['error'],
-    'no-confusing-arrow': [
-      'error',
-      {
-        allowParens: true,
-        onlyOneSimpleParam: true
-      }
-    ],
-    'no-duplicate-imports': [
-      'error',
-      {
-        includeExports: true
-      }
-    ],
-    'no-eval': ['error'],
-    'no-lone-blocks': ['error'],
-    'no-return-assign': ['error'],
-    'no-script-url': ['error'],
-    'no-self-compare': ['error'],
-    'no-unmodified-loop-condition': ['error'],
-    'no-unneeded-ternary': ['error'],
-    'no-use-before-define': ['error'],
-    'no-useless-call': ['error'],
-    'no-useless-return': ['error'],
-    'no-var': ['error'],
-    'object-shorthand': ['error', 'consistent-as-needed'],
-    'prefer-arrow-callback': ['warn'],
-    'prefer-const': ['error'],
-    'prefer-regex-literals': [
-      'error',
-      {
-        disallowRedundantWrapping: true
-      }
-    ],
-    quotes: [
-      'error',
-      'single',
-      {
-        avoidEscape: true,
-        allowTemplateLiterals: false
-      }
-    ],
-    'sort-imports': [
-      'warn',
-      {
-        ignoreCase: false,
-        ignoreDeclarationSort: false,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        allowSeparatedGroups: false
-      }
-    ],
-    yoda: ['error', 'never']
+import js from '@eslint/js'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import pluginPromise from 'eslint-plugin-promise'
+
+export default [
+  js.configs.recommended,
+  eslintPluginPrettierRecommended,
+  pluginPromise.configs['flat/recommended'],
+  {
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: ['@getify/proper-arrows', 'promise'],
+    rules: {
+      '@getify/proper-arrows/params': [
+        'error',
+        {
+          unused: 'trailing',
+          count: 3,
+          length: 0
+        }
+      ],
+      '@getify/proper-arrows/this': ['warn', 'never'],
+      'arrow-body-style': ['error', 'as-needed'],
+      'arrow-parens': ['warn'],
+      'comma-dangle': ['error', 'never'],
+      curly: ['error', 'all'],
+      'default-case-last': ['error'],
+      'dot-notation': ['error'],
+      eqeqeq: ['error', 'always'],
+      'no-array-constructor': ['error'],
+      'no-confusing-arrow': [
+        'error',
+        {
+          allowParens: true,
+          onlyOneSimpleParam: true
+        }
+      ],
+      'no-duplicate-imports': [
+        'error',
+        {
+          includeExports: true
+        }
+      ],
+      'no-eval': ['error'],
+      'no-lone-blocks': ['error'],
+      'no-return-assign': ['error'],
+      'no-script-url': ['error'],
+      'no-self-compare': ['error'],
+      'no-unmodified-loop-condition': ['error'],
+      'no-unneeded-ternary': ['error'],
+      'no-use-before-define': ['error'],
+      'no-useless-call': ['error'],
+      'no-useless-return': ['error'],
+      'no-var': ['error'],
+      'object-shorthand': ['error', 'consistent-as-needed'],
+      'prefer-arrow-callback': ['warn'],
+      'prefer-const': ['error'],
+      'prefer-regex-literals': [
+        'error',
+        {
+          disallowRedundantWrapping: true
+        }
+      ],
+      quotes: [
+        'error',
+        'single',
+        {
+          avoidEscape: true,
+          allowTemplateLiterals: false
+        }
+      ],
+      'sort-imports': [
+        'warn',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: false
+        }
+      ],
+      yoda: ['error', 'never']
+    }
   }
-}
+]
--- packages/eslint-config-vue/eslint-config-vue.js
+++ packages/eslint-config-vue/eslint-config-vue.js
@@ -1,34 +1,38 @@
-module.exports = {
-  extends: ['plugin:vue/vue3-recommended'],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['vue'],
-  rules: {
-    'sort-imports': [
-      'warn',
-      {
-        ignoreCase: false,
-        ignoreDeclarationSort: false,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        allowSeparatedGroups: true
-      }
-    ],
-    'vue/max-attributes-per-line': [
-      'warn',
-      {
-        singleline: {
-          max: 25
-        },
-        multiline: {
-          max: 1
+import pluginVue from 'eslint-plugin-vue'
+
+export default [
+  ...pluginVue.configs['flat/recommended'],
+  {
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    plugins: ['vue'],
+    rules: {
+      'sort-imports': [
+        'warn',
+        {
+          ignoreCase: false,
+          ignoreDeclarationSort: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: true
         }
-      }
-    ],
-    'vue/html-self-closing': 'off',
-    'vue/singleline-html-element-content-newline': 'off'
+      ],
+      'vue/max-attributes-per-line': [
+        'warn',
+        {
+          singleline: {
+            max: 25
+          },
+          multiline: {
+            max: 1
+          }
+        }
+      ],
+      'vue/html-self-closing': 'off',
+      'vue/singleline-html-element-content-newline': 'off'
+    }
   }
-}
+]
